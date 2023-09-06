d7e7f4e7d029c6b61d2673c0b498d9b89c46b059
KushalRB <25602111+KushalRB@users.noreply.github.com>
2023-09-06 18:49:58 +0545

    Works with an array of strings

--- Dominant Language Recognizer.playground/Contents.swift
+++ Dominant Language Recognizer.playground/Contents.swift
@@ -1,10 +1,27 @@
 import NaturalLanguage
 
 let sampleText = "मेरो नाम कुशल हो।"
+let languageDictionary = []
+let textArray = [
+                "My name is Kushal",
+                "मेरो नाम कुशल हो।",
+                "il mio nome è Kushal",
+                "私の名前はクシャルです",
+                "ನನ್ನ ಹೆಸರು ಕುಶಾಲ್",
+                "我的名字是库沙尔",
+                "mi nombre es kushal",
+                "je m'appelle Kushal",
+                "میرا نام کشال ہے۔",
+                "Mein Name ist Kushal"
+                ]
 
-
-if let language = NLLanguageRecognizer.dominantLanguage(for: sampleText){
-    print("Detected \(language.rawValue.uppercased()) as dominant language for \n\(sampleText)")
-}else{
-    print("Could not recognize language for \(sampleText)")
+for text in textArray{
+    if let language = NLLanguageRecognizer.dominantLanguage(for: text){
+        let langStr = Locale.current
+        print("Detected \(language.rawValue.uppercased()) as dominant language for \n\(text)\n")
+    }else{
+        print("Could not recognize language for \(text)")
+    }
 }
+
+
