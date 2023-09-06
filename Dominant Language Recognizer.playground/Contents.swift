82158cb2508872ac0d4db5bdecc6c52caf917f10
KushalRB <25602111+KushalRB@users.noreply.github.com>
2023-09-06 17:47:27 +0545

    Dominant Language in a string detected

--- Dominant Language Recognizer.playground/Contents.swift
+++ Dominant Language Recognizer.playground/Contents.swift
@@ -1,3 +1,10 @@
-import UIKit
+import NaturalLanguage
 
-var greeting = "Hello, playground"
+let sampleText = "मेरो नाम कुशल हो।"
+
+
+if let language = NLLanguageRecognizer.dominantLanguage(for: sampleText){
+    print("Detected \(language.rawValue.uppercased()) as dominant language for \n\(sampleText)")
+}else{
+    print("Could not recognize language for \(sampleText)")
+}
