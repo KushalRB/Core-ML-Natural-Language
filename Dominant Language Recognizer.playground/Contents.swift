d3248b980d1ddf307c4a268d7346c8e95d21b9d1
KushalRB <25602111+KushalRB@users.noreply.github.com>
2023-09-08 15:22:55 +0545

    CSV file 1. Load a CSV file 2. Process the csv file through dataframes

--- Dominant Language Recognizer.playground/Contents.swift
+++ Dominant Language Recognizer.playground/Contents.swift
@@ -1,7 +1,6 @@
 import NaturalLanguage
+import TabularData
 
-let sampleText = "मेरो नाम कुशल हो।"
-let languageDictionary = []
 let textArray = [
                 "My name is Kushal",
                 "मेरो नाम कुशल हो।",
@@ -15,13 +14,32 @@ let textArray = [
                 "Mein Name ist Kushal"
                 ]
 
-for text in textArray{
-    if let language = NLLanguageRecognizer.dominantLanguage(for: text){
-        let langStr = Locale.current
-        print("Detected \(language.rawValue.uppercased()) as dominant language for \n\(text)\n")
-    }else{
-        print("Could not recognize language for \(text)")
+if let fileURL = Bundle.main.url(forResource: "LanguageCodes", withExtension: "csv") {
+    do {
+        let languageDataframe = try DataFrame(contentsOfCSVFile: fileURL)
+        print(languageDataframe)
+        findDominantLanguage(languageDataframe: languageDataframe)
+    } catch {
+        print("Error: \(error)")
+    }
+} else {
+    print("Error: Unable to locate the CSV file in the app's bundle.")
+}
+
+
+func findDominantLanguage(languageDataframe : DataFrame){
+    for text in textArray{
+        if let language = NLLanguageRecognizer.dominantLanguage(for: text){
+            let detectedLanguage = languageDataframe.filter(on: "CODE", String.self, {$0 == language.rawValue})
+            print(detectedLanguage.columns[1])
+            print("Detected \(language.rawValue) as dominant language for \n\(text)\n")
+        }else{
+            print("Could not recognize language for \(text)")
+        }
     }
 }
+
+
+
 
 
