a1d96daf7638e32ff3afda261d0d0be812a2cc79
KushalRB <25602111+KushalRB@users.noreply.github.com>
2023-09-12 14:23:44 +0545

    Tokenised a string

--- String Tokeniser.playground/Contents.swift
+++ String Tokeniser.playground/Contents.swift
@@ -1,3 +1,10 @@
-import UIKit
+import NaturalLanguage
 
-var greeting = "Hello, playground"
+let text = "You might not think that programmers are artists, but programming is an extremely creative profession. It is logic-based creativity"
+
+let tagger = NLTagger(tagSchemes: [.tokenType])
+tagger.string = text
+tagger.enumerateTags(in: text.startIndex..<text.endIndex, unit: .word, scheme: .tokenType, options: [.omitWhitespace, .omitPunctuation]) { tag, range in
+    print(text[range])
+    return true
+}
