72613ce1dc9c502f24647d50cb0a6b972a0e5fbf
KushalRB <25602111+KushalRB@users.noreply.github.com>
2023-09-12 18:39:00 +0545

    Code implemented for recognizing parts of speech

--- SpeechPartsIdentifier.playground/Contents.swift
+++ SpeechPartsIdentifier.playground/Contents.swift
@@ -1,3 +1,17 @@
-import UIKit
+import NaturalLanguage
 
-var greeting = "Hello, playground"
+let text = "You might not think that programmers are artists, but programming is an extremely creative profession. It is logic-based creativity."
+
+let tagger = NLTagger(tagSchemes: [.lexicalClass, .language, .script])
+tagger.string = text
+tagger.enumerateTags(in: text.startIndex..<text.endIndex,
+                     unit: NLTokenUnit.word,
+                     scheme: NLTagScheme.lexicalClass,
+                     options: [.omitWhitespace,.omitPunctuation]) {
+                    (tag, range) -> Bool in
+                        print(text[range])
+                        print(tag?.rawValue ?? "unknown")
+                        print("\n")
+                        return true
+                    }
+
